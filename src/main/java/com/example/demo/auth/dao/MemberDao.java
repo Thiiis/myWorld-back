2153057d0e4f32fef44d958beb94afcd62bbe17c
soyoung park <nada981226@gmail.com>
2025-10-06 19:37:00 +0900

    auth/refactor: @Login떼고 updatePwd가 본인의 pwd만 update 할 수 있도록 변경 - auth/feat: random으로 미니홈 방문하는  기능 추가 - auth/refactor: memberUpdateRequest에서 mid 제거, httpservelet으로 받을 수 있도록 변경

--- src/main/java/com/example/demo/auth/dao/MemberDao.java
+++ src/main/java/com/example/demo/auth/dao/MemberDao.java
@@ -1,7 +1,6 @@
 package com.example.demo.auth.dao;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -14,9 +13,10 @@ import com.example.demo.common.dto.ProfileInfo;
 public interface MemberDao {
      public void insert(Member member);
      public Member selectByAccount(String account);
-     public Optional<Member> selectByMid(Long mid);
+     public Member selectByMid(Long mid);
      List<ProfileInfo> searchMembers(@Param("keyword") String keyword, @Param("loginMid") Long loginMid);
      public void update(@Param("mid") Long mid, @Param("pwd") String pwd);
      public int deleteByMid(Long mid);
      public int countByAccount(String account);
+     public int countAll();
 }
